af94ca980adbf6e8af330e35761bf23fc97897e7
banjo_dracer <63825976+dracer001@users.noreply.github.com>
2026-03-27 02:06:33 +0100

    Change app to use PORT environment variable
    Updated the app to use a dynamic port from environment variables.

--- app.py
+++ app.py
@@ -56,5 +56,6 @@ def checkhealth():
 
 if __name__ == '__main__':
     init_file()
-    # Run on port 5000, accessible to the network
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Render provides a PORT environment variable
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
